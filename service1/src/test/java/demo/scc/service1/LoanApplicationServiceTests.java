c953ef7a5c6ae5f8324fbdf38ae40224346b7b15
perenesenko <andrii.perenesenko@gmail.com>
2018-01-09 11:51:40 -0800

    Done! write tests for service1 that uses Stub of service2

--- service1/src/test/java/demo/scc/service1/LoanApplicationServiceTests.java
+++ service1/src/test/java/demo/scc/service1/LoanApplicationServiceTests.java
@@ -1,15 +1,35 @@
 package demo.scc.service1;
 
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author Andrii Perenesenko
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@AutoConfigureStubRunner(ids = {"demo.scc.service2"})
+@AutoConfigureStubRunner(ids = {"demo.scc:service2:+:stubs:8080"}, workOffline = true)
 public class LoanApplicationServiceTests {
+
+    private RestTemplate restTemplate = new RestTemplate();
+
+    @Test
+    public void testService2Stub() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Content-Type", MediaType.APPLICATION_JSON_UTF8_VALUE);
+        ResponseEntity<String> response = restTemplate.exchange("http://localhost:8080/fraudcheck", HttpMethod.PUT,
+                new HttpEntity<>("{\"clientId\":\"1234567890\",\"loanAmount\":99999}", httpHeaders),
+                String.class);
+        Assertions.assertThat(response.getBody()).isEqualTo("{\"fraudCheckStatus\":\"FRAUD\",\"rejectionReason\":\"Amount too high\"}");
+    }
 }
